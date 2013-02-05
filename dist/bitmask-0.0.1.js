6811bb0f9c8ca668cf1f096c5110ef3a83109e67
Stephen Young <young.steveo@gmail.com>
2013-02-05 07:49:03 -0500

    Removed '.js' from project names to make the dist files' names look better.

--- dist/bitmask-0.0.1.js
+++ dist/bitmask-0.0.1.js
@@ -1,5 +1,5 @@
 /**
- * bitmask.js v0.0.1 - 2013-02-05
+ * bitmask v0.0.1 - 2013-02-05
  * Quick and painless bitmasks for controlling application state.
  *
  * Copyright (c) 2013 Stephen Young <young.steveo@gmail.com>
@@ -7,12 +7,25 @@
  */
 ;(function(undefined){
     "use strict";
-    var Bitmask, tags;
+    var Bitmask, tags, index, slice, pow;
 
+    index = 0;
     tags = {};
+    slice = Array.prototype.slice;
+    pow = Math.pow;
+
+    /**
+     * [Bitmask description]
+     */
     Bitmask = function(){};
+
+    /**
+     * [set description]
+     */
     Bitmask.prototype.set = function(){};
-    Bitmask.prototype.isset = function(){};
+    Bitmask.prototype.all = function(){};
+    Bitmask.prototype.any = function(){};
+    Bitmask.prototype.get = function(){};
     Bitmask.prototype.inspect = function(){};
     if (typeof exports !== 'undefined' && typeof module !== 'undefined' && module.exports) {
         exports = module.exports = Bitmask;
